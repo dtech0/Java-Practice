6c6a833af8b999752cd694443a0cf2e1b5c78cbf
Md. Sibgatur Rahman Efti <sibgaturrahmanbros@gmail.com>
2024-09-28 19:50:21 +0600

    Initial commit

---
+++ Product.java
@@ -0,0 +1,25 @@
+public class Product{
+    public static void main(String[] args){
+
+        // First task:
+
+        // System.out.println("Id: 101");
+        // System.out.println("Title: iphone 15");
+        // System.out.println("Price: 1895 euros");
+        // System.out.println("Description: Perfect product with best image qualiy");
+        // System.out.println("Category: phone");
+
+        // Second task:
+
+        int id = 101;
+        String title = "iphone 15";
+        double price = 1895.0;
+        String description = "Perfect product with best image qualiy";
+        String category = "phone";
+        System.out.println("Id: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Price: " + price + " euros");
+        System.out.println("Description: " + description);
+        System.out.println("Category: " + category);
+    }
+}
