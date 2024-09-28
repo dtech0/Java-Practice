332611a2aa98a834e629a116c3f656fcccce3632
Md. Sibgatur Rahman Efti <sibgaturrahmanbros@gmail.com>
2024-09-29 00:50:03 +0600

    getting input from user

--- Product.java
+++ Product.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Product{
     public static void main(String[] args){
 
@@ -11,15 +13,50 @@ public class Product{
 
         // Second task:
 
-        int id = 101;
-        String title = "iphone 15";
-        double price = 1895.0;
-        String description = "Perfect product with best image qualiy";
-        String category = "phone";
+        // int id = 101;
+        // String title = "iphone 15";
+        // double price = 1895.0;
+        // String description = "Perfect product with best image qualiy";
+        // String category = "phone";
+        // System.out.println("Id: " + id);
+        // System.out.println("Title: " + title);
+        // System.out.println("Price: " + price + " euros");
+        // System.out.println("Description: " + description);
+        // System.out.println("Category: " + category);
+
+        // Third task:
+
+        int id;
+        String title;
+        double price;
+        String description;
+        String category;
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the id: ");
+        id = input.nextInt();
+        input.nextLine(); // It is used when user tap enter it takes grasp the new line
+        
+        System.out.print("Enter the Title: ");
+        title = input.nextLine();
+        
+        System.out.print("Enter the Price: ");
+        price = input.nextDouble();
+        input.nextLine(); // It is used when user tap enter it takes grasp the new line
+        
+        System.out.print("Enter the description: ");
+        description = input.nextLine();
+        
+        System.out.print("Enter the category: ");
+        category = input.nextLine(); // It is used when user tap enter it takes grasp the new line
+
         System.out.println("Id: " + id);
         System.out.println("Title: " + title);
         System.out.println("Price: " + price + " euros");
         System.out.println("Description: " + description);
         System.out.println("Category: " + category);
+
+        input.close();  // for preventing the resource leak problem
     }
 }
