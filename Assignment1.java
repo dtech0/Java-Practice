aedbdac6b9f74accd189bdff515e0493773e87d9
Md. Sibgatur Rahman Efti <sibgaturrahmanbros@gmail.com>
2024-09-29 11:37:28 +0600

    separate files

---
+++ Assignment1.java
@@ -0,0 +1,9 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("Id: 101");
+        System.out.println("Title: iphone 15");
+        System.out.println("Price: 1895 euros");
+        System.out.println("Description: Perfect product with best image qualiy");
+        System.out.println("Category: phone");
+    }
+}
---
+++ Assignment2.java
@@ -0,0 +1,14 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+        int id = 101;
+        String title = "iphone 15";
+        double price = 1895.0;
+        String description = "Perfect product with best image qualiy";
+        String category = "phone";
+        System.out.println("Id: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Price: " + price + " euros");
+        System.out.println("Description: " + description);
+        System.out.println("Category: " + category);
+    }
+}
